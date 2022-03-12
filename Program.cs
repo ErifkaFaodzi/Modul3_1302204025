ed65849a1bcf8b0bd1e71244495bad9d6406b6c1
ErifkaFaodzi <erifka@student.telkomuniversity.ac.id>
2022-03-12 12:36:25 +0700

    ConsoleApp1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        KodeBuah.NamaBuah inputNamaBuah1= KodeBuah.NamaBuah.Apel;
+        KodeBuah.NamaBuah inputNamaBuah2= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah3= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah4= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah5= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah6= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah7= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah8= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah8= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah9= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah10= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah11= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah12= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah13= KodeBuah.NamaBuah.Durian;
+        KodeBuah.NamaBuah inputNamaBuah13= KodeBuah.NamaBuah.Durian;
+
+        Console.WriteLine("Buah : " + inputNamaBuah + " Kode : " + KodeBuah.getKodeBuah(inputNamaBuah));
+        Console.WriteLine("Buah : " + inputNamaBuah1 + " Kode : " + KodeBuah.getKodeBuah(inputNamaBuah1));
+    }
+}
